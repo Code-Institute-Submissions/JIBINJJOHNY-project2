f9d652b8c48e7c4ad6f883342653780667afb0b5
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-07-25 15:14:24 +0200

    Add function to shuffle the alphabet array

--- assets/js/alphabets.js
+++ assets/js/alphabets.js
@@ -159,3 +159,12 @@ function nextLetter() {
     isShowingImage = false;
     displayCurrentLetter();
 }
+/** Function to Shuffle the Alphabet Array
+ * The shuffleAlphabet function shuffles the arr array using the Fisher-Yates algorithm to randomize the order of the alphabet data.
+ */
+function shuffleAlphabet(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
