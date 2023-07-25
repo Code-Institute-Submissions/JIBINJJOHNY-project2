3cc0eb8331a55263142a6b12ada7498f5a58aebf
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-07-25 13:01:29 +0200

    Add rferences to the necessary elements from html page to menu.js file

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+// Get references to the necessary elements from the HTML page
+const openRulesButton = document.getElementById('openrules_btn');
+const closeModalButton = document.getElementById('closemodal_btn');
+const redirectButton = document.getElementById("redirect_btn");
+const alphabetsButton = document.getElementById("alphabets_btn");
+const gameButton = document.getElementById("game_btn");
+const rulesModal = document.getElementById('rules_model');
